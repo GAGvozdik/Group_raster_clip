84f15b4755e9f2e30c98089f50b97c56b4a1702b
GAGvozdik <69218434+GAGvozdik@users.noreply.github.com>
2022-10-15 22:32:13 +0300

    V_01

--- 0_1.py
+++ 0_1.py
@@ -4,48 +4,26 @@ from arcpy import env
 from arcpy.sa import *
 
 
-
-
-
-#
 input = arcpy.GetParameterAsText(0) + "/"
 contour = arcpy.GetParameterAsText(1)
 output = arcpy.GetParameterAsText(2) + "/"
 
-# input = "C:/Users/fenik/Desktop/000_ANTON/"
-# contour = "C:/Users/fenik/Desktop/000_ANTON/contour.shp"
-# output = "C:/Users/fenik/Desktop/000_ANTON/output/"
-
-
+# input = "C:/Users/fenik/Desktop/RAN/GroupTIFFcut/Group_raster_clip/input/"
+# contour = "C:/Users/fenik/Desktop/RAN/GroupTIFFcut/Group_raster_clip/contour.shp"
+# output = "C:/Users/fenik/Desktop/RAN/GroupTIFFcut/Group_raster_clip/output/"
 
 
 if os.path.exists(output):
     for f in os.listdir(output):
         os.remove(os.path.join(output, f))
 
-
-
-
 i = 0
-c = 0
 end_var = []
 for root, dirs, files in os.walk(input):
     for filename in files:
-
         if str(filename[-4:]) == '.tif':
 
             end_var.append(files[i])
-            print end_var
-
-
-            for j in end_var:
-                l = 0
-                if filename == j:
-                    l+=1
-                    break
-
-            if l == 1 :
-                break
 
             arcpy.Clip_management(
                 input + filename,
@@ -54,7 +32,7 @@ for root, dirs, files in os.walk(input):
                 contour,
                 "0",
                 "ClippingGeometry")
-
+            print 'grd2'
 
         i += 1
         if i > len(files):
