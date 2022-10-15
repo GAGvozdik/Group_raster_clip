2e3d1c2ce53cb054a14cd3f41794dcb1a33d6ea2
GAGvozdik <69218434+GAGvozdik@users.noreply.github.com>
2022-10-15 23:33:00 +0300

    V_02

--- 0_1.py
+++ 0_1.py
@@ -13,17 +13,61 @@ output = arcpy.GetParameterAsText(2) + "/"
 # output = "C:/Users/fenik/Desktop/RAN/GroupTIFFcut/Group_raster_clip/output/"
 
 
-if os.path.exists(output):
-    for f in os.listdir(output):
-        os.remove(os.path.join(output, f))
+
 
 i = 0
 end_var = []
+
+
+
+
 for root, dirs, files in os.walk(input):
     for filename in files:
         if str(filename[-4:]) == '.tif':
 
             end_var.append(files[i])
+
+            for root1, dirs1, files1 in os.walk(output):
+                for filename1 in files1:
+
+                    n = output + filename[:-4] + "_cliped.tif"
+                    if filename1 == n[len(output):]:
+                        os.remove(n)
+
+                    n = output + filename[:-4] + "_cliped.tif.aux.xml"
+                    if filename1 == n[len(output):]:
+                        os.remove(n)
+
+                    n = output + filename[:-4] + "_cliped.tif.ovr"
+                    if filename1 == n[len(output):]:
+                        os.remove(n)
+
+                    n = output + filename[:-4] + "_cliped.tfw"
+                    if filename1 == n[len(output):]:
+                        os.remove(n)
+
+                    n = output + filename[:-4] + "_cliped.tif.xml"
+                    if filename1 == n[len(output):]:
+                        os.remove(n)
+
+            for root1, dirs1, files1 in os.walk(output):
+                for filename1 in files1:
+                    if filename1 == output + filename[:-4] + "_cliped" + filename[-4:]:
+                        n = output + filename[:-4] + "_cliped" + filename[-4:]
+                        os.remove(n)
+                        n = output + filename[:-4] + "_cliped.tif.aux.xml"
+                        os.remove(n)
+                        n = output + filename[:-4] + "_cliped.tif.ovr"
+                        os.remove(n)
+
+                        n = output + filename[:-4] + "_cliped.tfw"
+                        os.remove(n)
+                        n = output + filename[:-4] + "_cliped.tif.ovr"
+                        os.remove(n)
+                        n = output + filename[:-4] + "_cliped.tif.xml"
+                        os.remove(n)
+                        print "gg"
+
 
             arcpy.Clip_management(
                 input + filename,
@@ -32,7 +76,7 @@ for root, dirs, files in os.walk(input):
                 contour,
                 "0",
                 "ClippingGeometry")
-            print 'grd2'
+
 
         i += 1
         if i > len(files):
